0853bdfdbb3b0664dd838f7ca4a66c03949a58e2
anamarialoga <anamarialogaa82@gmail.com>
2022-03-30 17:03:03 +0300

    bugg

--- backEnd/server.js
+++ backEnd/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const path = require('path');
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 connectDB(); //connect to database
 const app = express();
 
